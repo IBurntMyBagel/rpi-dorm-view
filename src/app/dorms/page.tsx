1746ea359d106b702d5949da7be01212252de0ff
AndrewJZeng <andrewjiashuzeng@gmail.com>
2026-03-24 23:15:41 -0400

    Started working on dorm list UI

--- src/app/dorms/page.tsx
+++ src/app/dorms/page.tsx
@@ -1,9 +1,108 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        RPI Dorm View - Dorm List
-      </h1>
+    <main className="flex-col h-screen">
+      {/* this goes behind the header so it doesn't look like whitespace */}
+      <div className="h-18 bg-black flex">
+
+      </div>
+
+      <div className="min-h-screen flex justify-center">
+        {/* sidebar, where the filter is located */}
+        <div className="w-1/6 bg-gray-800 text-white p-4 py-8">
+          <h2 className="text-center text-xl font-bold mb-4">
+            Filter Options
+          </h2>
+
+          {/* list of filter options */}
+          <p className="text-center text-lg my-4">Year</p>
+          <ul className="space-y-4">
+            <li className="flex items-center gap-2">
+              <input type="checkbox" className="w-4 h-4" />
+              <p>Freshmen</p>
+            </li>
+
+            <li className="flex items-center gap-2">
+              <input type="checkbox" className="w-4 h-4" />
+              <p>Sophomore</p>
+            </li>
+
+            <li className="flex items-center gap-2">
+              <input type="checkbox" className="w-4 h-4" />
+              <p>Junior</p>
+            </li>
+
+            <li className="flex items-center gap-2">
+              <input type="checkbox" className="w-4 h-4" />
+              <p>Senior</p>
+            </li>
+          </ul>
+
+          <div className="h-px bg-white my-4"></div>
+
+          <p className="text-center text-lg my-4">Room Type</p>
+          <ul className="space-y-4">
+            <li className="flex items-center gap-2">
+              <input type="checkbox" className="w-4 h-4" />
+              <p>Single</p>
+            </li>
+
+            <li className="flex items-center gap-2">
+              <input type="checkbox" className="w-4 h-4" />
+              <p>Double</p>
+            </li>
+
+            <li className="flex items-center gap-2">
+              <input type="checkbox" className="w-4 h-4" />
+              <p>Triple</p>
+            </li>
+
+            <li className="flex items-center gap-2">
+              <input type="checkbox" className="w-4 h-4" />
+              <p>Suite</p>
+            </li>
+          </ul>
+        </div>
+
+        {/* dorm list main body */}
+        <div className="w-5/6 bg-gray-100 p-6 space-y-4">
+          <h1 className="text-center text-4xl font-bold">
+            Dorms
+          </h1>
+
+          {/* dorm list */}
+          <ul className="space-y-4">
+            <li className="flex p-4 bg-gray-400 rounded space-x-4">
+              <img src="/fieldrpi.jpg" alt="86 field" className="w-48 h-48 object-cover rounded" />
+              <div className="flex flex-col w-full space-y-4">
+                <div className="text-lg font-bold">Dorm: Crockett Hall</div>
+
+                <div className="grid grid-cols-3 gap-4">
+                  <p>Nearest dining hall: Commons</p>
+                  <p>A/C: No</p>
+                  <p>Elevator: No</p>
+                </div>
+
+                <p className="font-bold">Tags: ethernet, bike rack, elevator</p>
+              </div>
+            </li>
+
+            <li className="flex p-4 bg-gray-400 rounded space-x-4">
+              <img src="/fieldrpi.jpg" alt="86 field" className="w-48 h-48 object-cover rounded" />
+              <div className="flex flex-col w-full space-y-4">
+                <div className="text-lg font-bold">Dorm: Quad</div>
+
+                <div className="grid grid-cols-3 gap-4">
+                  <p>Nearest dining hall: Sage</p>
+                  <p>A/C: No</p>
+                  <p>Elevator: No</p>
+                </div>
+
+                <p className="font-bold">Tags: ethernet, bike rack, elevator</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </main>
   )
 }
