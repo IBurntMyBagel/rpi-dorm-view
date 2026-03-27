e2a9dbb9f340dc15ad828df5afbfb7dd15c29f6c
Sean Curow <sacurow@gmail.com>
2026-03-27 16:59:20 -0400

    initialized branch
    created function files and headers, some notes

---
+++ src/app/dorms/query_database.js
@@ -0,0 +1,5 @@
+export function query_database(filter_values){
+	//logic here
+	//will need to talk to andrew about format of filter_values as input
+	//expecting return value to be exactly what the db responds with
+}
---
+++ src/app/dorms/translate_response.js
@@ -0,0 +1,7 @@
+export function translate_response(db_response){
+	let dormlist = [];
+
+	//fill in with translation logic
+
+	return dormlist;
+}
