89dd8766f0914ba2114eadb2ddd1c7bc23ed4a66
rock2202 <linr11@rpi.edu>
2026-04-02 23:18:04 -0400

    fixed redirecting bug on vercel and created /lib

--- src/app/dorms/page.tsx
+++ src/app/dorms/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useState, useEffect } from "react";
-import { DormBuilding } from "../DormBuilding"
+import { DormBuilding } from "../../lib/DormBuilding"
 
 export default function Home() {
 
